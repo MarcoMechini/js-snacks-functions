fe0f4326c49a4a9d3e5df3ac4b63ad713d5cac45
Marco Mechini <mechinimarco35@gmail.com>
2024-11-06 15:33:18 +0100

    feat: es 2 complete

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,31 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
+let firstChar = 'A'
 // Dichiara la funzione qui.
 
 
+/**
+ * Passo come parametri un array e un char e per ogni volta che viene trovato un riscontro tra il char 
+ * e la prima lettera di ogni elemento dell'aray viene salvato in un nuovo array
+ *
+ * @param {*} namesArr
+ * @param {*} char
+ * @returns {{}}
+ */
+const alfabetArray = (namesArr, char) => {
+    char = char.toUpperCase();
+    const alfabetArray = [];
+    for (let i = 0; i < namesArr.length; i++) {
+        const curName = namesArr[i];
+        curName[0] === char ? alfabetArray.push(curName) : '';
+    }
+    return alfabetArray
+}
+
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(alfabetArray(names, firstChar));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
