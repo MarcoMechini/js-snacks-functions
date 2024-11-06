87aa3785e0589124ca97e69cac8c5afd3ddbf6b5
Marco Mechini <mechinimarco35@gmail.com>
2024-11-06 15:10:14 +0100

    reafactoring: first es with function params

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,45 +1,36 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const namesFirstChar = [];
 
 // Dichiara la funzione qui.
 
-/**
- * Prende il primo carattere di ogni elemento
- *
- * @param {string} word
- * @return {string} //char
- */
-// function firstChar(word) {
-//     return word[0]
-// }
-
-const firstChar = (word) => word[0];
-
-
-// function saveChar() {
+// function saveChar(nameArr) {
+//     const firstChar = [];
 //     for (let i = 0; i < names.length; i++) {
-//         const curName = names[i];
-//         namesFirstChar.push(firstChar(curName));
+//         const curName = names[i][0];
+//         firstChar.push(curName);
 //     }
+//     return firstChar
 // }
 
 /**
  * Salva i char dentro un array
  *
- * @return {string} //???
+ * @param {Array} //???
+ * @return {Array} //???
  */
-const saveChar = () => {
-    for (let i = 0; i < names.length; i++) {
-        namesFirstChar.push(firstChar(names[i]));
+const saveChar = (nameArr) => {
+    const firstChar = [];
+    for (let i = 0; i < nameArr.length; i++) {
+        firstChar.push(nameArr[i][0]);
     }
+    return firstChar;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-saveChar();
+saveChar(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
 
-console.log(namesFirstChar);
+console.log(saveChar(names));
